1a845b99b815cf2168b7c2fef0bd7f3f1fdd5fd2
Tomi Junnila <topeju@badzilla.net>
2025-03-24 17:35:45 +0200

    Hopefully fix enable pin operation

--- esphome/components/dm556/dm556.cpp
+++ esphome/components/dm556/dm556.cpp
@@ -10,8 +10,8 @@ void DM556::setup() {
   ESP_LOGCONFIG(TAG, "Setting up DM556...");
   if (this->enable_pin_ != nullptr) {
     this->enable_pin_->setup();
-    this->enable_pin_->digital_write(true);
-    this->enable_pin_state_ = true;
+    this->enable_pin_->digital_write(false);
+    this->enable_pin_state_ = false;
   }
   this->step_pin_->setup();
   this->step_pin_->digital_write(false);
@@ -28,10 +28,10 @@ void DM556::dump_config() {
 void DM556::loop() {
   bool at_target = this->has_reached_target();
   if (this->enable_pin_ != nullptr) {
-    bool enable_falling_edge = enable_pin_state_ & at_target;
-    this->enable_pin_->digital_write(at_target);
-    this->enable_pin_state_ = at_target;
-    ESP_LOGD("dm556", "Enable pin set to %d", at_target);
+    bool enable_falling_edge = enable_pin_state_ && !at_target;
+    this->enable_pin_->digital_write(!at_target);
+    this->enable_pin_state_ = !at_target;
+    ESP_LOGD("dm556", "Enable pin set to %d", !at_target);
     if (enable_falling_edge) {
       delayMicroseconds(1000);
     }
@@ -47,9 +47,11 @@ void DM556::loop() {
     return;
 
   this->dir_pin_->digital_write(dir == 1);
-  delayMicroseconds(50);
+
+  // TODO: Should this instead use ledc (PWM)?
+  delayMicroseconds(50); // 5 us setup time for the DIR signal is required --> this delay could be reduced
   this->step_pin_->digital_write(true);
-  delayMicroseconds(5);
+  delayMicroseconds(5); // Minimum pulse width 2.5 us --> doubled
   this->step_pin_->digital_write(false);
 }
 
